a4dfebe3e0e0eb761729384d14d26427575483e4
choqueuse <vincent.choqueuse@enib.fr>
2020-03-08 20:38:13 +0100

    truc

--- python/generate_csv.py
+++ python/generate_csv.py
@@ -1,59 +1,60 @@
-import numpy as np
-from scipy import signal
-import matplotlib.pyplot as plt
+from ENIB_lib import *
 
 system_list=[]
 freq_list=[]
 
 # system1
-w0=2*np.pi*10000
-m=0.5
-system1 = signal.lti([2*m*1/w0,0], [(1/(w0**2)),2*m/w0,1])
-system_list.append(system1)
-freq_list.append({"min":3,"max":5})
+sys = LP(2,0.2,1000)
+w, mag, phase = sys.bode(w=np.logspace(2,4,1000))
+export_bode("../data/data0.csv",w,mag,phase)
 
 #system2
-w0=1640
-system2 = signal.lti([4], [(1/(w0**2)),2*0.2/w0,1])
-system_list.append(system2)
-freq_list.append({"min":1,"max":4})
+sys = LP(10,2,100)
+w, mag, phase = sys.bode(w=np.logspace(1,4,1000))
+export_bode("../data/data1.csv",w,mag,phase)
 
 #system3
-tau=1.06*(10**-5)
-system3 = signal.lti([-tau,1], [tau,1])
-system_list.append(system3)
-freq_list.append({"min":1,"max":5})
+sys = LP(1,0.8,500)
+w, mag, phase = sys.bode(w=np.logspace(1,5,1000))
+export_bode("../data/data2.csv",w,mag,phase)
 
 #system4
-w0=2*np.pi*1000
-system4 = signal.lti([1/(w0**2),0,0], [(1/(w0**2)),2*0.714/w0,1])
-system_list.append(system4)
-freq_list.append({"min":2,"max":5})
+sys = HP(10,2,100)
+w, mag, phase = sys.bode(w=np.logspace(1,4,1000))
+export_bode("../data/data3.csv",w,mag,phase)
 
 #system5
-w0=2*np.pi*100
-system5 = signal.lti([2,0], [(1/(w0**2)),2*0.1/w0,1])
-system_list.append(system5)
-freq_list.append({"min":1,"max":4})
+sys = HP(4,0.1,100)
+w, mag, phase = sys.bode(w=np.logspace(0,5,1000))
+export_bode("../data/data4.csv",w,mag,phase)
 
 #system6
-w0=2*np.pi*10
-system6 = signal.lti([5], [(1/(w0**2)),2*1.2/w0,1])
-system_list.append(system6)
-freq_list.append({"min":0,"max":4})
+sys = BP(0.5,0.25,2000)
+w, mag, phase = sys.bode(w=np.logspace(2,5,1000))
+export_bode("../data/data5.csv",w,mag,phase)
 
 #system7
-w0=2*np.pi*20
-system7 = signal.lti([3], [1/w0,1])
-system_list.append(system7)
-freq_list.append({"min":-1,"max":4})
+sys = BP(5,1,100)
+w, mag, phase = sys.bode(w=np.logspace(0,4,1000))
+export_bode("../data/data6.csv",w,mag,phase)
 
-for indice in range(len(system_list)):
-    system=system_list[indice]
-    freq=freq_list[indice]
-    f=np.logspace(freq["min"],freq["max"],1000)
-    w,T=system.freqresp(2*np.pi*f)
-    filename="../data/data{}.csv".format(indice)
-    header="f, module, argument"
-    data=np.transpose([f,np.abs(T),180*np.angle(T)/np.pi])
-    np.savetxt(filename,data,header=header, delimiter=",",comments="")
+#system7
+sys = BP(5,4,200)
+w, mag, phase = sys.bode(w=np.logspace(2,5,1000))
+export_bode("../data/data7.csv",w,mag,phase)
+
+
+#system7
+sys = Notch(10,5,100)
+w, mag, phase = sys.bode(w=np.logspace(1,4,1000))
+export_bode("../data/data8.csv",w,mag,phase)
+
+#system7
+sys = Notch(1,0.05,1000)
+w, mag, phase = sys.bode(w=np.logspace(2,4,1000))
+export_bode("../data/data9.csv",w,mag,phase)
+
+#system7
+sys = Notch(2,0.9,100)
+w, mag, phase = sys.bode(w=np.logspace(1,4,1000))
+export_bode("../data/data10.csv",w,mag,phase)
